c9908d120613b4b711f507703425381572fbec5f
eder0782 <eder0782@gmail.com>
2023-07-21 13:48:11 -0400

    feat: init project

---
+++ src/app.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { TasksModule } from './tasks/tasks.module';
+import { AuthModule } from './auth/auth.module';
+import { AuthModule } from './auth/auth.module';
+import { TasksModule } from './tasks/tasks.module';
+import { UserModule } from './user/user.module';
+
+@Module({
+  imports: [UserModule, AuthModule, TasksModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
